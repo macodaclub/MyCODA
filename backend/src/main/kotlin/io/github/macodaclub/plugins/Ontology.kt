81f3f1607483b6b76a8cc689359d73441c261a50
tiagonuneslx <tiago.nunes.lx@gmail.com>
2024-10-15 15:27:36 +0100

    Allow loading the ontology from local file

--- backend/src/main/kotlin/io/github/macodaclub/plugins/Ontology.kt
+++ backend/src/main/kotlin/io/github/macodaclub/plugins/Ontology.kt
@@ -8,21 +8,42 @@ import org.semanticweb.owlapi.model.OWLOntologyManager
 import org.semanticweb.owlapi.reasoner.structural.StructuralReasonerFactory
 import org.semanticweb.owlapi.util.OWLOntologyMerger
 import org.swrlapi.factory.SWRLAPIFactory
+import java.io.File
 
 class OntologyManager {
-    private val mycodaOntologyUrl =
-        System.getenv("MYCODA_ONTOLOGY_URL") ?: error("Please provide environment variable MYCODA_ONTOLOGY_URL")
+    private val mycodaOntologyUrl = System.getenv("MYCODA_ONTOLOGY_URL")
+    private val mycodaOntologyFilePath = System.getenv("MYCODA_ONTOLOGY_FILE_PATH")
     private var manager: OWLOntologyManager = OWLManager.createOWLOntologyManager()
-    var mycodaOntology = manager.loadOntology(IRI.create(mycodaOntologyUrl))
+    var mycodaOntology: OWLOntology
     var mergedOntology: OWLOntology = OWLOntologyMerger(manager).createMergedOntology(manager, IRI.create("https://www.merged-ontology.com"))
     var reasoner = StructuralReasonerFactory().createReasoner(mergedOntology)
     var sqwrlQueryEngine = SWRLAPIFactory.createSQWRLQueryEngine(mergedOntology)
     private val lemmatize: String.() -> String = /* configureCoreNLP() */ { contextReceiver() }
     var entityFinder = EntityFinder(this, lemmatize)
 
+    init {
+        mycodaOntology = if(mycodaOntologyUrl != null) {
+            manager.loadOntology(IRI.create(mycodaOntologyUrl))
+        } else {
+            if(mycodaOntologyFilePath != null) {
+                manager.loadOntologyFromOntologyDocument(File(mycodaOntologyFilePath))
+            } else {
+                error("Please provide environment variable MYCODA_ONTOLOGY_URL or MYCODA_ONTOLOGY_FILE_PATH")
+            }
+        }
+    }
+
     fun reloadOntology() {
         val manager = OWLManager.createOWLOntologyManager()
-        mycodaOntology = manager.loadOntology(IRI.create(mycodaOntologyUrl))
+        mycodaOntology = if(mycodaOntologyUrl != null) {
+            manager.loadOntology(IRI.create(mycodaOntologyUrl))
+        } else {
+            if(mycodaOntologyFilePath != null) {
+                manager.loadOntologyFromOntologyDocument(File(mycodaOntologyFilePath))
+            } else {
+                error("Please provide environment variable MYCODA_ONTOLOGY_URL or MYCODA_ONTOLOGY_FILE_PATH")
+            }
+        }
         mergedOntology =
             OWLOntologyMerger(manager).createMergedOntology(manager, IRI.create("https://www.merged-ontology.com"))
         reasoner = StructuralReasonerFactory().createReasoner(mergedOntology)
